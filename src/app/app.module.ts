4ab0d24bd3bf40cb4e59e0f5544c23883abb4823
prograTammz <ahmedtamer1998@gmail.com>
2020-05-03 21:52:52 +0200

    Added news article component with header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticleComponentComponent } from './article-component/article-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponentComponent
   ],
   imports: [
     BrowserModule,

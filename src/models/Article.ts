229392e93575e100f1fe86f2c16c88c38e212d38
Ahmed <Ahmed.h.elsabbagh@gmail.com>
2020-05-29 22:29:24 +0200

    fixed the annoying array of cells error

--- src/models/Article.ts
+++ src/models/Article.ts
@@ -1,8 +1,9 @@
 import { Cell } from "./Cell";
 
 export interface Article {
+  atricle_id: string;
   user_id: string;
-  cells: [Cell];
+  cells: Cell[];
   title: string;
   heading_image: string;
   heading_image_caption: string;
